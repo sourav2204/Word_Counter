b432e06e888ab9b6ed9076554cbfb16b6bbf3e4a
Sourav Panda <76568489+sourav2204@users.noreply.github.com>
2023-06-21 14:49:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Wordcounter from "./Components/Wordcounter";
+//import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Wordcounter />
+    </div>
+  );
+}
+
+export default App;
